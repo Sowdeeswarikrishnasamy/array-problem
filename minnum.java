4a099db06e90678af3d63662cbfc2edff1fa0f49
Sowdeeswari <sowdeeswari282@gmail.com>
2025-09-17 11:45:25 +0530

    minimum num in an arrray

---
+++ minnum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class minnum{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int []num=new int[size];
+        for(int ind=0;ind<size;ind++){
+            num[ind]=sc.nextInt();
+        }
+        int first=num[0];
+         for(int ind=0;ind<size;ind++){
+           if(first>num[ind])
+               first=num[ind];
+        } 
+        System.out.println("min num:"+first);
+        }
+        
+     }
